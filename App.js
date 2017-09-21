bcd16ea59938eae00798aa54f35ec01f07f5001f
davidmgudeman <davidmgudeman@gmail.com>
2017-09-21 11:33:29 -0700

    scaffolded out the Welcome, Auth and Companies Screens

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { TabNavigator } from 'react-navigation';
+import WelcomeScreen from './screens/WelcomeScreen';
+import AuthScreen from './screens/AuthScreen';
 
 export default class App extends React.Component {
   render() {
+
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
       </View>
     );
   }
---
+++ screens/CompaniesScreen.js
@@ -0,0 +1,19 @@
+
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class CompaniesScreen extends Component {
+  render() {
+    return (
+      <View>
+        <Text>CompaniesScreen</Text>
+        <Text>CompaniesScreen</Text>
+        <Text>CompaniesScreen</Text>
+        <Text>CompaniesScreen</Text>
+        <Text>CompaniesScreen</Text>
+      </View>
+    )
+  }
+}
+
+export default CompaniesScreen;
