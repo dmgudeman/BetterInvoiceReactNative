706d8db9a1296b69cf9d0ecb30ce8ddfa60a9d47
davidmgudeman <davidmgudeman@gmail.com>
2017-10-18 09:14:18 -0700

    worked on the authorization so that the token is retained. Code compiles, functionality has not been tested

--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -7,19 +7,19 @@ class WelcomeScreen extends Component {
   state = { token: null }
 
   async componentWillMount() {
-    // let token = await AsyncStorage.getItem('fb_token');
-    // console.log("WELCOME SCREEN 02 is token null or false ", (token === null || token === false ));
+    let token = await AsyncStorage.getItem('fb_token');
+    console.log("WELCOME SCREEN 02 is token null or false ", (token === null || token === false ));
 
 
-    // if (token) {
-    //   console.log("WELCOME SCREEN 03 In componenWilMOunt token =    ", token);
-    //   // this.setState({ token });
-    //   this.props.navigation.navigate('companies');
-    // } else {
-    //   console.log("WELCOME SCREEN 04 In componenWilMOunt4     ", token);
+    if (token) {
+      console.log("WELCOME SCREEN 03 In componenWilMOunt token =    ", token);
+      // this.setState({ token });
+      this.props.navigation.navigate('companies');
+    } else {
+      console.log("WELCOME SCREEN 04 In componenWilMOunt4     ", token);
       // this.setState({ token: false })
       this.props.navigation.navigate('auth');
-    // }
+    }
   }
 
 
